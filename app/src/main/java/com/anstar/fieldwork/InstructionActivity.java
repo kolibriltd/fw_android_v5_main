36c94a7dc94638802e88e7b1a103d0a4d9a522f4
Oleg Dmitriv <odmitriv@gmail.com>
2015-07-22 10:17:40 +0300

    Modify InstructionActivity make extends AppCompatActivity

--- app/src/main/java/com/anstar/fieldwork/InstructionActivity.java
+++ app/src/main/java/com/anstar/fieldwork/InstructionActivity.java
@@ -2,14 +2,15 @@ package com.anstar.fieldwork;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
-import android.text.Html;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.widget.TextView;
 
 import com.anstar.common.Const;
 import com.anstar.models.AppointmentInfo;
 import com.anstar.models.list.AppointmentModelList;
 
-public class InstructionActivity extends BaseActivity {
+public class InstructionActivity extends AppCompatActivity {
 
 	int appointment_id = 0;
 	AppointmentInfo appointmentInfo = null;
@@ -18,19 +19,28 @@ public class InstructionActivity extends BaseActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.instruction);
+		setContentView(R.layout.activity_instruction);
 		Bundle b = getIntent().getExtras();
 		if (b != null) {
 			if (b.containsKey(Const.Appointment_Id)) {
 				appointment_id = b.getInt(Const.Appointment_Id);
 			}
 		}
+/*
 		ActionBar action = getSupportActionBar();
 		action.setTitle(Html.fromHtml("<font color='"
 				+ getString(R.string.header_text_color)
 				+ "'>Instructions</font>"));
 		action.setHomeButtonEnabled(true);
 		action.setDisplayHomeAsUpEnabled(true);
+*/
+		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+		setSupportActionBar(toolbar);
+
+		ActionBar action = getSupportActionBar();
+		action.setDisplayHomeAsUpEnabled(true);
+		action.setDisplayShowHomeEnabled(true);
+
 		appointmentInfo = AppointmentModelList.Instance().getAppointmentById(
 				appointment_id);
 		txtInstruction = (TextView) findViewById(R.id.txtInstruction);
